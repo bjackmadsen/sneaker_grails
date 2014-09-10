32c2398c991dc9020ee4a6c4333fd7f906f19f09
Brandon Madsen <bjackmadsen@gmail.com>
2014-09-10 16:17:34 -0700

    add a new brand name to home page

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -3,4 +3,22 @@ class BrandsController < ApplicationController
 		@brands = Brand.all 
 		render('brands/index.html.erb')
 	end
+
+	def new
+		@brands = Brand.all
+		@brand = Brand.new
+		render('brands/new.html.erb')
+	end
+
+	def create
+		@brands = Brand.all 
+		@brand = Brand.new(params[:brand])
+		if @brand.save
+			flash[:notice] = "The shoe brand has been added!"
+			redirect_to('/brands')
+		else
+			render('/brands/new.html.erb')
+		end
+	end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  match('/', {:via => :get, :to => 'brands#index'})
   match('brands', {:via => :get, :to => 'brands#index'})
+  match('brands/new', {:via => :get, :to => 'brands#new'})
+  match('brands', {:via => :post, :to => 'brands#create'})
 end
