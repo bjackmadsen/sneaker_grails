a8db48bc093f1772e72cfc7e1349634257212bfe
Brandon Madsen <bjackmadsen@gmail.com>
2014-12-18 16:14:00 -0800

    fixing heroku app

--- app/models/style.rb
+++ app/models/style.rb
@@ -6,9 +6,13 @@ class Style < ActiveRecord::Base
 	validates :content, :presence => true
 	validates :brand_id, :presence => true
 	has_attached_file :photo, :styles => { :medium => "900x600" },
-					  :url => ":s3_domain_url",
-					  :path => "/:class/:attachment/:id_partition/:style/:filename"
+					  :url => "/assets/images/:id/:style/:basename.:extension",
+					  :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension"
 	validates_attachment_presence :photo
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+	attr_accessor :photo_file_name
+	attr_accessor :photo_content_type
+	attr_accessor :photo_file_size
+	attr_accessor :photo_updated_at
 	belongs_to :brand 
 end
