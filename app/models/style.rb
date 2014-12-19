e43374c542f65a34742eb45894a3017c4cb2b595
Brandon Madsen <bjackmadsen@gmail.com>
2014-12-18 18:29:47 -0800

    changed the style model to save images to s3

--- app/models/style.rb
+++ app/models/style.rb
@@ -6,9 +6,16 @@ class Style < ActiveRecord::Base
 	validates :content, :presence => true
 	validates :brand_id, :presence => true
 	has_attached_file :photo, :styles => { :medium => "900x600" },
-					  :url => "/assets/images/:id/:style/:basename.:extension",
-					  :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension"
-	validates_attachment_presence :photo
+					  :storage => :s3,
+    				  :bucket => ENV['S3_BUCKET_NAME'],
+    				  :path => "/images/:id/:style/:basename.:extension",
+    				  :url => ":s3_domain_url",
+      				  :s3_credentials => {
+      				  	:bucket => ENV['S3_BUCKET_NAME'],
+    					:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    					:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    				  }
+	validates :photo, :attachment_presence => true
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 	belongs_to :brand 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
   # Specify the AWS configuration variables for the production Environment.
   config.paperclip_defaults = {
   :storage => :s3,
+  :bucket => ENV['S3_BUCKET_NAME'],
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
